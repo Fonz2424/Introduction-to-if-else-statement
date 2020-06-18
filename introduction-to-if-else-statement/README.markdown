ea01973472433fd6f864c8715c079b70123c6920
Fonz2424 <65732402+Fonz2424@users.noreply.github.com>
2020-06-18 15:09:51 -0400

    Introduction to if else statement

---
+++ introduction-to-if-else-statement/README.markdown
@@ -0,0 +1,5 @@
+# Introduction to if else statement
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Fonz2424/pen/JjGEGJP](https://codepen.io/Fonz2424/pen/JjGEGJP).
+
+
---
+++ introduction-to-if-else-statement/dist/script.js
@@ -0,0 +1,17 @@
+//declare variable name variable jamesAge, set jamesAge to 21
+
+var jamesAge = 21;
+
+//use if-else-statement to check jame's age
+//if jame's age is > or = to 21...
+
+if(jamesAge >=21){
+    
+    //print string that reads: james can drink alcohol
+    console.log("james can drink alcohol");  
+    
+   //print string that reads: james cannot drink alcohol 
+    } else {
+  
+    console.log("james cannot drink alcohol"); 
+}
